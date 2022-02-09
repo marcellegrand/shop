66faae0153fdbc42e5d50ef4f19577f3b097f336
marcellegrand <marcel.lazodelavega@gmail.com>
2022-02-08 20:55:37 -0500

    Deployment: 05 -> AWS

--- shop/settings.py
+++ shop/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7!((w=y3e*@u9e9h-h4!n@uv$l*0f5a^d==9(z4@ppej5$scbh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
